e0c000c5d43da9a4240baa403cce44f9dd9c8a04
Andre Hitchman <madetobethebest@hotmail.co.uk>
2018-01-05 21:04:11 +0000

    remove api interface from presenter

--- app/src/main/java/com/example/android/funnymems/Presenter/MemeListPresenterImpl.java
+++ app/src/main/java/com/example/android/funnymems/Presenter/MemeListPresenterImpl.java
@@ -1,11 +1,9 @@
 package com.example.android.funnymems.Presenter;
 
-import com.example.android.funnymems.domain.GetMemesUseCaseCallback;
-import com.example.android.funnymems.domain.GetMemesUseCaseImpl;
 import com.example.android.funnymems.View.MemeListActivity;
 import com.example.android.funnymems.app.model.Meme;
-import com.example.android.funnymems.app.net.MemeApi;
-import com.example.android.funnymems.app.net.MemeApiClient;
+import com.example.android.funnymems.domain.GetMemesUseCaseCallback;
+import com.example.android.funnymems.domain.GetMemesUseCaseImpl;
 
 import java.util.List;
 
@@ -13,45 +11,17 @@ public class MemeListPresenterImpl implements MemeListPresenter, GetMemesUseCase
 
     private final MemeListActivity view;
     private final GetMemesUseCaseImpl getMemesUseCase;
-    private MemeApi memeApiInterface;
 
     public MemeListPresenterImpl(MemeListActivity view) {
         this.view = view;
         this.getMemesUseCase = new GetMemesUseCaseImpl();
-        this.memeApiInterface = MemeApiClient.getClient().create(MemeApi.class);
     }
 
     @Override
     public void getMemes() {
         getMemesUseCase.execute(view);
-//        Call<MemeData> call = memeApiInterface.getMemeList();
-//
-//        call.enqueue(new Callback<MemeData>() {
-//            @Override
-//            public void onResponse(Call<MemeData> call, Response<MemeData> response) {
-//                Memes memesList = response.body().getMemeData();
-//                List<Meme> memes = new ArrayList<>();
-//                List<Meme> meme = memesList.getMemes();
-//
-//                for (Meme memeItem : meme) {
-//                    memes.add(new Meme(memeItem.id,
-//                            memeItem.name,
-//                            memeItem.url,
-//                            memeItem.width,
-//                            memeItem.height));
-//                }
-//
-//                view.onMemesFetched(memes);
-//            }
-//
-//            @Override
-//            public void onFailure(Call<MemeData> call, Throwable t) {
-//                Log.e(t.getMessage(), "Error fetching memes.");
-//            }
-//        });
     }
 
-    @Override
     public void onMemesRetrieved(List<Meme> memes) {
         //getMemesUseCase.execute();
     }
